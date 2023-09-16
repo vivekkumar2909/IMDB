6c8cd11015ff7c676415ae4b156dd378e053f055
vivekkumar2909 <rajvivek620065@gmail.com>
2023-09-16 15:02:12 +0530

    Done

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,25 +1,9 @@
-// import React from 'react'
-
-// export default function Navbar() {
-//   return (
-//     <div className='flex gap-10 justify-between bg-slate-400  items-center  font-bold text-2xl h-20 hover:cursor-pointer dark: bg-smoke-600 '>
-//       <div className=' ml-60  hover:text-amber-500'>
-//         Trending
-//       </div>
-//       <div className=' mr-60  hover:text-amber-500  '>
-//         Latest
-//       </div>
-//     </div>
-//   )
-// }/
-
-
 import NavbarItem from "./NavbarItem";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between  dark:bg-teal-600 bg-gray-600 text-white lg:text-lg p-4 border border-black">
-      <NavbarItem title="Trending" param="fetchTrending"  />
+    <div className=" flex justify-center md:justify-around md:40 sm:justify-around sm:text-sm dark:bg-teal-700 bg-gray-700 lg:text-lg p-4">
+      <NavbarItem title="Trending" param="fetchTrending" />
       <NavbarItem title="Top Rated" param="fetchTopRated" />
     </div>
   );
--- src/app/components/NavbarItem.js
+++ src/app/components/NavbarItem.js
@@ -10,10 +10,10 @@ export default function NavbarItem({ title, param }) {
   return (
     <div>
       <Link
-        className={` m-56 hover:text-amber-600 font-bold p-2 ${
+        className={` m-5 sm:justify-around  text-xl  hover:text-amber-600 font-semibold p-2 ${
           genre &&
           genre === param &&
-          "underline underline-offset-8 decoration-1  decoration-amber-500 rounded-lg"
+          "underline underline-offset-8   decoration-amber-500 rounded-lg"
         }`}
         href={`/?genre=${param}`}
       >
